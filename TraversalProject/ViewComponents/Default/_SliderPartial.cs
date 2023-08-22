30c81538e4f183d77c9198080e5826df7067a771
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-08-22 17:07:50 +0300

    Layout düzenlemeleri-15

---
+++ TraversalProject/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalProject.ViewComponents.Default
+{
+    public class _SliderPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
