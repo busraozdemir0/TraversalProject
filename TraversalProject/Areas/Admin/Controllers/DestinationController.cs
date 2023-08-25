4c2df648ce0e578e876cc78dd3e435e226521ec8
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-08-25 17:38:54 +0300

    Admin Area Layout düzenlemeleri ve Destinasyon(Rota) işlemleri - 51

---
+++ TraversalProject/Areas/Admin/Controllers/DestinationController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Index", "Destination", new { area = "Admin" });
+        }
+        public IActionResult DeleteDestination(int id)
+        {
+            var destinationID=destinationManager.TGetByID(id);
+            destinationManager.TDelete(destinationID);
+            return RedirectToAction("Index", "Destination", new { area = "Admin" });
+        }
+        [HttpGet]
+        public IActionResult UpdateDestination(int id)
+        {
+            var destinationID = destinationManager.TGetByID(id);
+            return View(destinationID);
+        }
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            destinationManager.TUpdate(destination);
+            return RedirectToAction("Index", "Destination", new { area = "Admin" });
+        }
+    }
+}
--- TraversalProject/Startup.cs
+++ TraversalProject/Startup.cs
@@ -80,6 +80,14 @@ namespace TraversalProject
                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
             });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
