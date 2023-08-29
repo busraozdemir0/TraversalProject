5a599e8504cc13819f73d3b6b0a321c34925038f
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-08-29 14:13:07 +0300

    Mail gönderme işlemi

--- TraversalProject/Areas/Admin/Controllers/DashboardController.cs
+++ TraversalProject/Areas/Admin/Controllers/DashboardController.cs
@@ -3,6 +3,7 @@
 namespace TraversalProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("Admin/[controller]/[action]/{id?}")]
     public class DashboardController : Controller
     {
         public IActionResult Index()
---
+++ TraversalProject/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,43 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using TraversalProject.Models;
+
+namespace TraversalProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]/{id?}")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "uiswagger@gmail.com"); // mail gönderenin bilgileri
+            mimeMessage.From.Add(mailboxAddressFrom);
+            
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);  // maili alacak olanın bilgileri
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            mimeMessage.Subject = mailRequest.Subject;  // mailin konusu
+
+            var bodyBuilder = new BodyBuilder();  // mailin içerik kısmı
+            bodyBuilder.TextBody= mailRequest.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();  
+
+            SmtpClient client=new SmtpClient();
+            client.Connect("smtp.gmail.com", 587,false); //mailin port numarası 587
+            client.Authenticate("uiswagger@gmail.com", "afhltikzagkfeusv"); // mail hesabındaki uygulama şifreleri kısmına girerek aldığımız kod
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            return RedirectToAction("Index", "Dashboard", new {area="Admin"});
+        }
+    }
+}
