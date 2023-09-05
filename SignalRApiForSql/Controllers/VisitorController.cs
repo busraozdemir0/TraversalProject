d844f76414859eb5f52b33cf5b072bd87b64a7f0
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-09-05 14:57:20 +0300

    SignalR verilerinin MSSQL'e taşınması - 87

---
+++ SignalRApiForSql/Controllers/VisitorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System;
+using SignalRApiForSql.DAL;
+using SignalRApiForSql.Model;
+
+namespace SignalRApiForSql.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitorController : ControllerBase
+    {
+        private readonly VisitorService _visitorService;
+
+        public VisitorController(VisitorService visitorService)
+        {
+            _visitorService = visitorService;
+        }
+        [HttpGet]
+        public IActionResult CreateVisitor()
+        {
+            Random random = new Random();
+            Enumerable.Range(1, 10).ToList().ForEach(x =>  // 10 günün değerini 5 tane şehir için yapacağımızdan dolayı toplam 50 veri otomatik kaydedilecek
+            {
+                foreach (ECity item in Enum.GetValues(typeof(ECity)))
+                {
+                    var newVisitor = new Visitor
+                    {
+                        City = item,
+                        CityVisitCount = random.Next(100, 2000),
+                        VisitDate = DateTime.Now.AddDays(x)
+                    };
+                    _visitorService.SaveVisitor(newVisitor).Wait();
+                    System.Threading.Thread.Sleep(1000);// her bir ekleme saniyede 1 kere gerçekleşecek
+                }
+            });
+            return Ok("Ziyaretçiler başarılı bir şekilde eklendi.");
+        }
+    }
+}
---
+++ SignalRApiForSql/Model/VisitorService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using SignalRApiForSql.DAL;
+using SignalRApiForSql.Hubs;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRApiForSql.Model
+{
+    public class VisitorService
+    {
+        private readonly Context _context;
+        private readonly IHubContext<VisitorHub> _hubContext;
+
+        public VisitorService(Context context, IHubContext<VisitorHub> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+
+        public IQueryable<Visitor> GetList()
+        {
+            return _context.Visitors.AsQueryable();
+        }
+        public async Task SaveVisitor(Visitor visitor)
+        {
+            await _context.Visitors.AddAsync(visitor);
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("CallVisitorList", "aaa");
+        }
+        public List<VisitorChart> GetVisitorChartList()
+        {
+            List<VisitorChart> visitorCharts = new List<VisitorChart>();
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = "query";
+                command.CommandType = CommandType.Text;
+                _context.Database.OpenConnection();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        VisitorChart visitorChart = new VisitorChart();
+                        visitorChart.VisitDate = reader.GetDateTime(0).ToShortDateString();
+                        Enumerable.Range(1, 5).ToList().ForEach(x => { // 5 tane şehir olduğu için
+                            visitorChart.Counts.Add(reader.GetInt32(x));
+                        });
+                        visitorCharts.Add(visitorChart);
+                    }
+                }
+                _context.Database.CloseConnection();
+                return visitorCharts;
+            }
+        }
+    }
+}
