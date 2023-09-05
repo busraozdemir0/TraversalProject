50d6473ef80a1da4cb57d93f4492354c37f92d74
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-09-05 14:09:17 +0300

    SignalR bağlantı yapılandırması - 86

---
+++ SignalRApi/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRApi.DAL
+{
+    public class Context:DbContext // Postgre SQL ile veritabanı işlemleri
+    {
+        public Context(DbContextOptions<Context> options):base(options)
+        {
+
+        }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
---
+++ SignalRApi/Model/VisitorService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using SignalRApi.DAL;
+using SignalRApi.Hubs;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRApi.Model
+{
+    public class VisitorService
+    {
+        private readonly Context _context;
+        private readonly IHubContext<VisitorHub> _hubContext ;
+
+        public VisitorService(Context context, IHubContext<VisitorHub> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+
+        public IQueryable<Visitor> GetList()
+        {
+            return _context.Visitors.AsQueryable();
+        }
+        public async Task SaveVisitor(Visitor visitor)
+        {
+            await _context.Visitors.AddAsync(visitor);
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("CallVisitorList", "aaa");
+        }
+        public List<VisitorChart> GetVisitorChartList()
+        {
+            List<VisitorChart> visitorCharts=new List<VisitorChart>();
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = "select * from crosstab\r\n(\r\n'Select \"VisitDate\",\"City\",\"CityVisitCount\"\r\n\tFrom \"Visitors\"\r\n\tOrder By 1,2'\r\n) As ct(\"VisitDate\" date , City1 int, City2 int, City3 int, City4 int, City5 int);";
+                command.CommandType = CommandType.Text;
+                _context.Database.OpenConnection();
+                using(var reader=command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        VisitorChart visitorChart = new VisitorChart();
+                        visitorChart.VisitDate = reader.GetDateTime(0).ToShortDateString();
+                        Enumerable.Range(1, 5).ToList().ForEach(x=> { // 5 tane şehir olduğu için
+                            visitorChart.Counts.Add(reader.GetInt32(x));
+                        });
+                        visitorCharts.Add(visitorChart);
+                    }
+                }
+                _context.Database.CloseConnection();
+                return visitorCharts;
+            }
+        }
+    }
+}
