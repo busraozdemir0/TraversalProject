dc0128db7ed6a987aa24b76cafe60f11ad649283
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-09-06 12:53:03 +0300

    Rol atama işlemleri - 92

--- TraversalProject/Areas/Admin/Controllers/RoleController.cs
+++ TraversalProject/Areas/Admin/Controllers/RoleController.cs
@@ -1,6 +1,8 @@
-﻿using EntityLayer.Concrete;
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TraversalProject.Areas.Admin.Models;
@@ -12,10 +14,12 @@ namespace TraversalProject.Areas.Admin.Controllers
     public class RoleController : Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public RoleController(RoleManager<AppRole> roleManager)
+        public RoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -67,6 +71,50 @@ namespace TraversalProject.Areas.Admin.Controllers
             value.Name=updateRoleViewModel.RoleName;
             await _roleManager.UpdateAsync(value);
             return RedirectToAction("Index");
+        }
+
+        public IActionResult UserList()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+
+        // Kullanıcılara rol atama işlemi
+        [HttpGet]
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id.Equals(id));
+            TempData["userid"]=user.Id;  // kullanıcının id'aini taşıma işlemi
+            var roles = _roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user); // belirtilen kullanıcının rollerini getir
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+            foreach(var role in roles)
+            {
+                RoleAssignViewModel model = new RoleAssignViewModel();
+                model.RoleId = role.Id;
+                model.RoleName = role.Name;
+                model.RoleExist = userRoles.Contains(role.Name); // belirtilen rol o kullanıcı da varsa true yoksa false
+                roleAssignViewModels.Add(model);
+            }
+            return View(roleAssignViewModels);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(List<RoleAssignViewModel> model)  // bir kullanıcıya birden fazla rol atayabilmek için modeli List formatında gönderdik
+        {
+            var userid = (int)TempData["userid"]; // yukarıdan tempdata ile gönderdiğimiz id'yi burada çektik
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == userid);
+            foreach(var item in model)
+            {
+                if(item.RoleExist) // rol seçili ise bu rolü o user'a ata
+                {
+                    await _userManager.AddToRoleAsync(user, item.RoleName);
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user, item.RoleName); // eğer rol seçi değilse o rolü kullanıcıdan kaldır
+                }
+            }
+            return RedirectToAction("UserList");
         }
     }
 }
---
+++ TraversalProject/Areas/Admin/Models/RoleAssignViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TraversalProject.Areas.Admin.Models
+{
+    public class RoleAssignViewModel
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool RoleExist { get; set; } // bu rol kullanıcıda var mı 
+    }
+}
