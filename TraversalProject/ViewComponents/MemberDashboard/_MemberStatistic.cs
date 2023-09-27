479b0e5f9ad2b35bc0c98891230ac3e030b0d5d9
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-09-27 16:02:47 +0300

    Son düzenlemeler

--- TraversalProject/ViewComponents/MemberDashboard/_MemberStatistic.cs
+++ TraversalProject/ViewComponents/MemberDashboard/_MemberStatistic.cs
@@ -11,23 +11,17 @@ namespace TraversalProject.ViewComponents.MemberDashboard
 {
     public class _MemberStatistic : ViewComponent
     {
-        private readonly UserManager<AppUser> _userManager;
-
-        public _MemberStatistic(UserManager<AppUser> userManager)
-        {
-            _userManager = userManager;
-        }
-
         public IViewComponentResult Invoke()
         {
             using (var context = new Context())
             {
-                var userName = _userManager.FindByNameAsync(User.Identity.Name);
-                ViewBag.aktifRezv = context.Reservations.Include(x => x.Destination).Where(y => y.Status == "Onaylandı" && y.AppUserId == userName.Id).Count(); // onaylanan rezervasyon sayısı
-                ViewBag.geçmişRezv = context.Reservations.Include(x => x.Destination).Where(y => y.Status == "Geçmiş Rezervasyon" && y.AppUserId == userName.Id).Count(); // geçmiş rezervasyon sayısı
-                ViewBag.onayBekleyenRezv = context.Reservations.Include(x => x.Destination).Where(y => y.Status == "Onay Bekliyor" && y.AppUserId == userName.Id).Count(); // onay bekleyen rezervasyon sayısı
-                ViewBag.totalComment=context.Comments.Include(x=>x.AppUser).Where(x=>x.AppUserID== userName.Id).Count(); // giren kişinin yaptığı toplam yorum sayısı
-                ViewBag.notificationComment = context.Announcements.Count();
+                var userName = User.Identity.Name;
+                var userID = context.Users.Where(x => x.UserName == userName).Select(y => y.Id).FirstOrDefault();
+                ViewBag.aktifRezv = context.Reservations.Include(x => x.Destination).Where(y => y.Status == "Onaylandı" && y.AppUserId == userID).Count(); // onaylanan rezervasyon sayısı
+                ViewBag.geçmişRezv = context.Reservations.Include(x => x.Destination).Where(y => y.Status == "Geçmiş Rezervasyon" && y.AppUserId == userID).Count(); // geçmiş rezervasyon sayısı
+                ViewBag.onayBekleyenRezv = context.Reservations.Include(x => x.Destination).Where(y => y.Status == "Onay Bekliyor" && y.AppUserId == userID).Count(); // onay bekleyen rezervasyon sayısı
+                ViewBag.totalComment=context.Comments.Include(x=>x.AppUser).Where(x=>x.AppUserID == userID).Count(); // giren kişinin yaptığı toplam yorum sayısı
+                ViewBag.notificationCount = context.Announcements.Count();
                 ViewBag.totalTour = context.Destinations.Count();
                 ViewBag.totalGuide = context.Guides.Count();
                 ViewBag.totalMember =context.Users.Count();
